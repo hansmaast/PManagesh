c1c15599d4c77a353b9831046c3de5fa1d179025
Martin <martin.molvaer@gmail.com>
2020-09-22 13:28:37 +0200

    fix dupe project add homepage

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,41 +5,38 @@ import { Button } from "react-bootstrap";
 import { CreateProjectModal } from "../components/modals/CreateProjectModal";
 
 export default () => {
-
-  const [ projects, setProjects ] = useState( [] );
+  const [projects, setProjects] = useState([]);
   const [showModal, setShowModal] = React.useState(false);
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchProjects = async () => {
       const projectsFromDb = await getProjects();
-      setProjects( [ ...projects, ...projectsFromDb ] );
-    }
+      setProjects([...projects, ...projectsFromDb]);
+    };
     fetchProjects();
-  }, [] )
+  }, []);
 
   return (
-      <div>
-        <h4>Projects:</h4>
-        <ul>
-          {
-            projects.map( project => {
-              return (
-                  <Link to={ `projects/${ project.id }` } key={ project.id }>
-                    <li>{ project.name }</li>
-                  </Link>
-              )
-            } )
+    <div>
+      <h4>Projects:</h4>
+      <ul>
+        {projects.map((project) => {
+          return (
+            <Link to={`projects/${project.id}`} key={project.id}>
+              <li>{project.name}</li>
+            </Link>
+          );
+        })}
+      </ul>
+      <Button variant="primary" onClick={() => setShowModal(true)}>
+        Add new project
+      </Button>
 
-          }
-        </ul>
-        <Button variant="primary" onClick={() => setShowModal(true)}>
-          Add new project
-        </Button>
-
-        <CreateProjectModal
-            show={showModal}
-            onHide={() => setShowModal(false)}
-        />
-      </div>
+      <CreateProjectModal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        currentProjects={projects}
+      />
+    </div>
   );
-}
+};
