d9b1e4e04a1dcd09e9aad332ba8980dd838bb582
Martin <martin.molvaer@gmail.com>
2020-09-22 15:50:03 +0200

    add team add footer

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,13 +1,72 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row, Image, Card, CardDeck } from "react-bootstrap";
+import { User, GitHub } from "react-feather";
 
 const Team = () => {
   return (
-      <div>
-        <Container>
-          <h1>Martin Molvær, Hans Maast, Per-Kristian Vinje</h1>
-        </Container>
-      </div>
+    <div>
+      <Row className="mb-5">
+        <CardDeck>
+          <Col>
+            <Card style={{ width: "250px", height: "300px" }}>
+              <Card.Header>
+                <Card.Title>
+                  <User />
+                  Hans Maast
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>He likes coding and pils</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <GitHub />
+                <Card.Subtitle className="mt-1 text-muted">
+                  Github
+                </Card.Subtitle>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "250px", height: "300px" }}>
+              <Card.Header>
+                <Card.Title>
+                  <User />
+                  Per-Kristian Vinje
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>He likes guitar and bootstrap grid</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <GitHub />
+                <Card.Subtitle className="mt-1 text-muted">
+                  Github
+                </Card.Subtitle>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "250px", height: "300px" }}>
+              <Card.Header>
+                <Card.Title>
+                  <User />
+                  Martin Molvær
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>He likes food and icons</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <GitHub />
+                <Card.Subtitle className="mt-1 text-muted">
+                  Github
+                </Card.Subtitle>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </CardDeck>
+      </Row>
+    </div>
   );
 };
 
